8d0433d5a957a1199bd36a4ea5c0b2d0a9c4eed0
Grace G Pascarella <97993431+pascargr@users.noreply.github.com>
2025-08-13 18:19:04 -0400

    update install tools

--- install_tools.sh
+++ install_tools.sh
@@ -20,6 +20,25 @@ brew install --cask orion
 echo "Installing Zed"
 brew install --cask zed
 
-# install python to mac HS
-echo "Installing Python to Macintosh HD"
+# install python to /opt/homebrew/bin/python3
+echo "Installing Python /opt/homebrew/bin/python3"
 brew install python@3.13
+
+# install VS Code
+echo "Installing VS Code"
+brew install --cask visual-studio-code
+
+# install uv 
+echo "Installing uv"
+brew install uv
+
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+# Download and install Node.js:
+nvm install 22
+
+# install jupyterlab
+echo "Installing JupyterLab"
+brew install jupyterlab
